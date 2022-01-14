d93f21bdef85478f8c268bb4828b29f7e579c7c2
Joshua Edwards <BlackBeltJJE@gmail.com>
2022-01-13 21:40:47 -0700

    Changed Procfile back to hello from hello2

--- hello2.py
+++ hello2.py
@@ -1,11 +1,14 @@
-import os
-import logging
-from flask import Flask
-
-app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
-
-@app.route('/')
-def hello():
-    logging.debug("saying hello")
-    return 'Hello Joshua! Procfile test. '
+# =============================================================================
+# import os
+# import logging
+# from flask import Flask
+# 
+# app = Flask(__name__)
+# logging.basicConfig(level=logging.DEBUG)
+# 
+# @app.route('/')
+# def hello():
+#     logging.debug("saying hello")
+#     return 'Hello Joshua! Procfile test. '
+# 
+# =============================================================================
