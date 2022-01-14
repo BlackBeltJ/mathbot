e87b23b0f51b446b45565961df68daf434001e23
Joshua Edwards <BlackBeltJJE@gmail.com>
2022-01-14 08:30:14 -0700

    Saved hello2.py

--- hello2.py
+++ hello2.py
@@ -7,5 +7,5 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
-    logging.debug("saying hello")
+    logging.debug("saying hello2")
     return 'Hello Joshua! Procfile test. '
